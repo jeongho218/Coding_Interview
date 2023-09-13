ce282078128b08e165be542aeff082a30d3a485b
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-14 01:05:52 +0900

    Update 특정 문자 제거하기.js

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,10 +1,9 @@
 const solution = (my_string, letter) => {
-  let arr = [...my_string];
-  let a = [];
-  arr.forEach((element) => {
+  let arr = [];
+  [...my_string].forEach((element) => {
     if (element !== letter) {
-      a.push(element);
+      arr.push(element);
     }
   });
-  return a.join("");
+  return arr.join("");
 };
