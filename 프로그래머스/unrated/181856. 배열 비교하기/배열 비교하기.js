6e91245dbc6908f6c9a36e40e097e5ffdc672b15
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-17 15:03:13 +0900

    Update 배열 비교하기.js

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -1,23 +1,14 @@
 const solution = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
-    // 배열의 길이가 다르다면
     return arr1.length > arr2.length ? 1 : -1;
-  } // 배열의 길이가 같다면
+  } 
   else {
-    return arr1.reduce((a, b) => {
+    let arr1Sum = arr1.reduce((a, b) => {
       return a + b;
-    }, 0) >
-      arr2.reduce((a, b) => {
-        return a + b;
-      }, 0)
-      ? 1
-      : arr1.reduce((a, b) => {
-          return a + b;
-        }, 0) <
-        arr2.reduce((a, b) => {
-          return a + b;
-        }, 0)
-      ? -1
-      : 0;
+    }, 0);
+    let arr2Sum = arr2.reduce((a, b) => {
+      return a + b;
+    }, 0);
+    return arr1Sum > arr2Sum ? 1 : arr1Sum < arr2Sum ? -1 : 0;
   }
 };
