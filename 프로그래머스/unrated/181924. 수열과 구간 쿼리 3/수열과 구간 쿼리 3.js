082e7a5ba2b32781592296a09056f72e8f53aa7f
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-23 22:55:28 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,9 @@
+const solution = (arr, queries) => {
+  for (i = 0; i < queries.length; i++) {
+    [arr[queries[i][0]], arr[queries[i][1]]] = [
+      arr[queries[i][1]],
+      arr[queries[i][0]],
+    ];
+  }
+  return arr;
+};
