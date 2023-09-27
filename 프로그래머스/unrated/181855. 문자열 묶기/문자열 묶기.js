cf80e8fcdbd47ac59c7da6038cd13a3809797ab6
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-28 01:01:35 +0900

    [unrated] Title: 문자열 묶기, Time: 9.54 ms, Memory: 50.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.js
@@ -1,8 +1,12 @@
 const solution = (strArr) => {
-  let arr = Array(strArr.length).fill(0);
+  let max = strArr.map((v) => {
+    return (v = v.length);
+  });
+
+  let arr = Array(Math.max(...max) + 1).fill(0);
+
   strArr.forEach((v) => {
     return (arr[v.length] += 1);
   });
-
   return Math.max(...arr);
 };
