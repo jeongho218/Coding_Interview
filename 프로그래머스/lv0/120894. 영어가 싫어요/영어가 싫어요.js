7d7e3d8a40d29b1e94cd3ad23e6ddee7c448f3fa
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-09-28 23:03:52 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,13 +1,19 @@
 const solution = (my_string) => {
-  return +my_string
-    .replaceAll("zero", "0")
-    .replaceAll("one", "1")
-    .replaceAll("two", "2")
-    .replaceAll("three", "3")
-    .replaceAll("four", "4")
-    .replaceAll("five", "5")
-    .replaceAll("six", "6")
-    .replaceAll("seven", "7")
-    .replaceAll("eight", "8")
-    .replaceAll("nine", "9");
+  const numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  numbers.forEach((v, i) => {
+    my_string = my_string.replaceAll(v, i);
+  });
+
+  return +my_string;
 };
