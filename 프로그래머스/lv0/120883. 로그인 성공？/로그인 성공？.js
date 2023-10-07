23b222b2d3259323ccca5f646d9b942a6316b1bc
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-10-07 18:35:21 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,11 @@
+const solution = (id_pw, db) => {
+
+  for (i = 0; i < db.length; i++) {
+    if (db[i][0] === id_pw[0] && db[i][1] === id_pw[1]) {
+      return "login";
+    } else if (db[i][0] === id_pw[0] && db[i][1] !== id_pw[1]) {
+      return "wrong pw";
+    }
+  }
+  return "fail";
+};
