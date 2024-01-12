8772f7c3973ea3e17e4304eedfa610be83fe150c
Jeong Ho Jung <82637024+jeongho218@users.noreply.github.com>
2024-01-13 05:08:22 +0900

    [Bronze V] Title: A+B - 7, Time: 136 ms, Memory: 9520 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let count = input.shift();
+for (i = 1; i <= count; i++) {
+  let [a, b] = input[i - 1].split(' ');
+  console.log(`Case #${i}: ${Number(a) + Number(b)}`);
+}
