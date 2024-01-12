73d1047d7115e54758a753948680c4d0e2d41241
Jeong Ho Jung <82637024+jeongho218@users.noreply.github.com>
2024-01-13 04:57:36 +0900

    [Bronze V] Title: A+B - 4, Time: 136 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,6 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+input.forEach((v) => {
+  let [a, b] = v.split(' ');
+  console.log(Number(a) + Number(b));
+});
