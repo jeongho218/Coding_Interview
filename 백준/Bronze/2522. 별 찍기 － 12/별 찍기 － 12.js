01a98e58a815bdb0f8bba553aa9f9d3e4232d02a
Jeong Ho Jung <82637024+jeongho218@users.noreply.github.com>
2024-01-13 18:05:44 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 140 ms, Memory: 9656 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = Number(fs.readFileSync('/dev/stdin'));
+for (i = 1; i <= input; i++) {
+  console.log(`${' '.repeat(input - i)}${'*'.repeat(i)}`);
+}
+for (i = input - 1; i >= 1; i--) {
+  console.log(`${' '.repeat(input - i)}${'*'.repeat(i)}`);
+}
